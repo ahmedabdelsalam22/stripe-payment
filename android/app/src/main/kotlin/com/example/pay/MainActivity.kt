e789195b0246c01fc2fe22c5a1caaa27d0a49765
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-03-08 13:27:11 +0200

    reformat code

--- android/app/src/main/kotlin/com/example/pay/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/pay/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.pay
 
-import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterFragmentActivity() {
+class MainActivity : FlutterFragmentActivity() {
 }
